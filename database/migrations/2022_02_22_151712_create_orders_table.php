f6cb18a7401dca7505fad015721080329daecf08
tunglq17080 <tunglq17080>
2022-02-22 22:37:37 +0700

    Add migration

---
+++ database/migrations/2022_02_22_151712_create_orders_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id')->unsigned()->nullable();
+            $table->string('name');
+            $table->string('phone');
+            $table->string('address');
+            $table->date('date_order');
+            $table->double('total');
+            $table->string('payment');
+            $table->string('note');
+            $table->boolean('status');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
