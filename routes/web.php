5947a2139f6a863d4e9d33492af2d6a75bf1bfe6
tunglq17080 <tunglq17080>
2022-02-25 08:08:25 +0700

    add 3 page

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+Route::get('/checkout', function () {
+    return view('checkout');
+});
+Route::get('/product-detail', function () {
+    return view('product-detail');
 });
